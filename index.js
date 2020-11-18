15e0e2a874a459ea48d315ac184cfbb2ae818490
Nikhil Verma <vermaisat@gmail.com>
2020-11-18 19:48:42 +0530

    cors

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/sendmsg',(req,res)=>{
+app.post('/sendmsg',async (req,res)=>{
     console.log(req.body.sendTo)
     var to = req.body.sendTo;
     var msg = req.body.message;
     const accountSid = 'ACd8a79ed5db68888a1002e2f7e47f1d18'; 
     const authToken = '4ed5c7ba0abe2d9c50df416e4041d551'; 
-    const client = require('twilio')(accountSid, authToken); 
-    console.log(typeof to)
+    const client = await require('twilio')(accountSid, authToken); 
+   
     client.messages 
           .create({ 
              body: `${msg}`, 
@@ -26,7 +26,8 @@ app.post('/sendmsg',(req,res)=>{
              to: `${to}` 
            }) 
           .then(message => console.log(message.sid)) 
-          .done();
+          .done()
+          .catch((error)=>console.log(res.status(400).send("Sorry Data cannot be Submitted")))
 })
 
 app.listen(4000,()=>{
